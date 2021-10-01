dcd8bc306fe901825725b27c505fa34ec97be1e9
faorozco <faorozco@utp.edu.co>
2021-09-30 22:05:20 -0500

    ciclos

---
+++ src/components/Car.vue
@@ -0,0 +1,42 @@
+<template>
+  <h2>Coche</h2>
+    <p>Marca: {{brand}}</p>
+    <p>Modelo: {{model}}</p>
+    <p>Las potencias para transitar en zona urbana deben ser menor a 300CV</p>
+    <ul>
+        <template v-for= "(power, index) in powers" :key="index">
+            <li v-if="power<200">
+            {{power}}
+            </li>
+        </template>
+
+    </ul>
+    <p>Las potencias para transitar en carretera deben ser igual o mayor a 200CV</p>
+    <ul>
+        <template v-for= "(power, index) in powers" :key="index">
+            <li v-if="power>=200">
+            {{power}}
+            </li>
+        </template>
+
+    </ul>
+</template>
+
+<script>
+export default {
+    setup(){
+        const brand = "Renault";
+        const model = "Megane";
+        
+        const powers = [40, 80, 120, 160, 200, 240, 280, 320];
+        
+        return {
+            brand, model, powers
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
